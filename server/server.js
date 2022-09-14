cd4beea00ecd47a31c7bbacb3d87489a58554922
Isaac Ubani <isaacchimdi@gmail.com>
2022-09-15 00:35:57 +0100

    modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -16,6 +16,9 @@ app.use(cors());
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+app.get('/', (req, res) => {
+    res.json({ message: 'Chat Application Homepage' })
+});
 
 
 
