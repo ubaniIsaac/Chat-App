052e5b63cd9b5742f7f500f9e991bb0142fb7f8f
Isaac Ubani <isaacchimdi@gmail.com>
2022-11-02 15:39:53 +0100

    modified:   client/src/components/Home.js 	modified:   client/src/index.css

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,25 @@ import '../Home.css'
 
 const Home = () => {
     return (
-        <div className='home'>
+        <div className="home">
+            <section className="top-bar">
+                <nav className="flex items-center ml-auto">
+                    <a href="/signup" className="btn btn_primary uppercase ml-5">Register</a>
+                    <a href="/signin" className="btn btn_primary uppercase ml-5">Login</a>
+                </nav>
+            </section>
+
+            <main>
+                <div>
+                    <h2 className="text-5xl">
+                        Welcome to ICeU Chat App
+                    </h2>
+                    <p> Login / Register to get started</p>
+                </div>
+
+            </main>
+
+            {/* </div>
             <div className="home-container">
                 <div className="text">Hi,</div>
                 <ul className='welcome-list'>
@@ -15,9 +33,7 @@ const Home = () => {
             </div >
             <div className="links" >
                 <div ><a href="/signup">SIGNUP</a></div>
-                <div ><a href="/signin">SIGNIN</a></div>
-
-            </div >
+                <div ><a href="/signin">SIGNIN</a></div> */}
         </div>
     )
 }
